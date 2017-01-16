5889fd9c0347a40a58806a3384e3bcbcdaba7096
Chimi <chimi@example.com>
2017-01-17 04:06:52 +0600

    Bootstrap theme:makeup-varsity

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -102,18 +102,9 @@
           <!-- <a class="navbar-brand" href="index.html"><img src="assets/img/logo.png" alt="logo"></a> -->
         </div>
         <div id="navbar" class="navbar-collapse collapse">
-          <ul id="top-menu" class="nav navbar-nav navbar-right main-nav">
-            <li class="active"><a href="index.html">Home</a></li>            
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Courses <span class="fa fa-angle-down"></span></a>
-              <ul class="dropdown-menu" role="menu">
-                <li><a href="course.html">Course Archive</a></li>                
-                <li><a href="course-detail.html">Course Detail</a></li>                
-              </ul>
-            </li>           
-            <li><a href="schools.html">Schools</a></li>
-                      
-            <li><a href="contact.html">Contact</a></li>
+          <ul id="top-menu" class="nav navbar-nav navbar-right main-nav">           
+           
+             <li><a href="contact.html">Contact</a></li>
                            
             <li><a href="#" id="mu-search-icon"><i class="fa fa-search"></i></a></li>
           </ul>                     
