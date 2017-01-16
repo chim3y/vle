9658c218fd99c328bf6e1fdb1cd374baf9749d40
Chimi <chimi@example.com>
2017-01-17 03:36:05 +0600

    Bootstrap theme:makeup-varsity

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -143,8 +143,19 @@
     </div>
   </div>
   <!-- End search box -->
+  <!-- Start Slider -->
   
- 
+    <!-- Start single slider item -->
+    <div class="mu-slider-single">
+      <div class="mu-slider-img">
+        <figure>
+          <img src="assets/img/slider/1.jpg" alt="img">
+        </figure>
+      </div>
+    </div>
+    
+    <!-- Start single slider item -->
+   
   <!-- Start service  -->
   <section id="mu-service">
     <div class="container">
