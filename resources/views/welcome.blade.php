bc18d8ec180a9a987b6214de308fef75165355ca
Chimi <chimi@example.com>
2017-01-17 04:13:38 +0600

    Bootstrap theme:makeup-varsity

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -134,7 +134,22 @@
     <!-- Start single slider item -->
     
           <img src="assets/img/slider/1.jpg" alt="img">
-      
+  <div class="container">
+  <div class="row">
+        <div class="col-sm-6 col-sm-offset-3">
+            <div id="imaginary_container"> 
+                <div class="input-group stylish-input-group">
+                    <input type="text" class="form-control"  placeholder="Search" >
+                    <span class="input-group-addon">
+                        <button type="submit">
+                            <span class="glyphicon glyphicon-search"></span>
+                        </button>  
+                    </span>
+                </div>
+            </div>
+        </div>
+  </div>
+</div>
     
     <!-- Start single slider item -->
    
