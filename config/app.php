62aa7b0e1588b4332b3252570e38da1cf457bcfe
Chimi <chimi@example.com>
2017-03-08 23:25:26 +0600

    Laravel upgraded successfully

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         'Yajra\Datatables\DatatablesServiceProvider',
         Collective\Html\HtmlServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+        Laravel\Tinker\TinkerServiceProvider::class,
     
         
      
